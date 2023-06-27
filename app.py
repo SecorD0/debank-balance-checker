50acd3f31db7c81d4e575b88cd064e6913fee5d2
SecorD <25284138+SecorD0@users.noreply.github.com>
2023-06-28 00:37:29 +0300

    1.0.1 | Use the 'create_files' function in the 'app.py' file

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import asyncio
 
-import utils
 from functions.parse_balances import parse_balances
+from utils.create_files import create_files
 
 if __name__ == '__main__':
+    create_files()
     parse_balances(asyncio.new_event_loop())
